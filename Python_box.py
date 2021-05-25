16945d38990468003ed87919af376a0dbf0d2bbc
tchapman1140 <84735870+tchapman1140@users.noreply.github.com>
2021-05-25 13:21:56 -0400

    Initial Commit
    Creating a box

--- Python_box.py
+++ Python_box.py
@@ -1,1 +1,20 @@
+#displaying a box. Input the size of box
+#variables
+length = int()
+width = int()
+stars = str()
+
+#ask user for length and width
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))   
+
+stars = "*" + ((width - 2) * " ") + "*"
+
+#loop to draw the box
+for counter in range (0, length):
+    if counter == 0 or counter == (length - 1):
+        print("*" * width)
+    else:
+        print(stars)
+
 
